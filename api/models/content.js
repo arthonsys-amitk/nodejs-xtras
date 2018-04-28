8d8ba8d7884bf642668e786685733dd867e7048d
admin-PC\admin <arudev1521@gmail.com>
2018-04-28 13:01:58 +0530

    added functionality to send email from contact us api

--- api/models/content.js
+++ api/models/content.js
@@ -3,6 +3,11 @@
 var exportFuns = {},
     config     = require('../../config'),
     Mongo      = require('../../mongo');
+	
+var nodemailer = require('nodemailer');
+
+var admin_contact_email = "amit.kothari@arthonsys.com";
+
 // returns inserted contact data
 exportFuns.insert_contact_us = (data)=>{
   var insertPattern = { email: data.email, contact_number: data.phone_no, comment: data.comment };
@@ -13,10 +18,43 @@ exportFuns.insert_contact_us = (data)=>{
     return db.insert('contact_us', insertPattern);
   })
   .then(function(user){
-    db.close();
-    return user.ops[0];
+	  return exportFuns.sendContactEmail(data.email, data.phone_no, data.comment)
+	  .then(function(res){
+		  db.close();
+		  return user.ops[0];
+	  });
+  })
+  .then(function(final_res){
+	  return final_res;
   });
 };
+
+
+exportFuns.sendContactEmail = (email, phone_no, comment) => {
+	var transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+		user: 'amitkothari.as@gmail.com',
+		pass: 'Arthonsys@12345'
+	  }
+	});
+
+	var mailOptions = {
+	  from: 'Xtras Admin',
+	  to: "" + admin_contact_email,
+	  subject: 'Xtras: Comments from ' + email,
+	  html: "From: " + email + "<br/>Phone No.:" + phone_no + "<br/>Comments: <br/>" + comment
+	};
+	return transporter.sendMail(mailOptions)
+	.then(function(info){
+		console.log('Contact Email sent: ' + info.response);			
+		return 1;
+	}).catch(function(err){
+		console.log(err);
+		return 0;
+	});
+};
+
 exportFuns.find_privacy_policy = ()=>{
   let searchPattern = {
     key : 'Privacy and Policy'
